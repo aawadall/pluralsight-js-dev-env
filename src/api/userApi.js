13994b1b019766254061cd2f0e97299c05d86606
aawadall <aawadall@ualberta.ca>
2018-04-12 21:39:14 -0400

    End of lesson 11, full integration with mock json generation services

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,13 +1,28 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
 import chalk from 'chalk';
+
+const baseUrl = getBaseUrl();
+
 export function getUsers() {
     return get('users');
 }
 
+export function deleteUser(id) {
+    return del(`users/${id}`);
+    
+}
 function get(url) {
-    return fetch(url).then(onSuccess, onError);
+    return fetch(baseUrl + url).then(onSuccess, onError);
 }
 
+function del(url) {
+    console.log(chalk.yellow(`deleting user ${url}`)); // eslint-disable-line no-console
+    const request = new Request(baseUrl + url, {
+        method: 'DELETE'
+    });
+    return fetch(request).then(onSuccess, onError);
+}
 function onSuccess(response) {
     return response.json();
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users  via API call.
 getUsers().then(result => {
@@ -14,4 +14,16 @@ getUsers().then(result => {
                </tr>`
    });
     global.document.getElementById('users').innerHTML = userBody;
+
+    const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+    Array.from(deleteLinks, link => {
+        link.onclick =  function(event) {
+            const element = event.target;
+            event.preventDefault();
+            deleteUser(element.attributes["data-id"].value);
+            const row = element.parentNode.parentNode;
+            row.parentNode.removeChild(row);
+        };
+    });
 });
